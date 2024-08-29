7c175ab17530f468cfa1259daf8d88cb82a5e328
Isadora Amaral <isadoraferreiraamaraldasilva@gmail.com>
2024-08-29 11:02:07 -0300

    Add files via upload

---
+++ 03-aula03.py
@@ -0,0 +1,16 @@
+# variaveis em python
+# não há necessidade de declará-las no inicio do programa
+# basta criar no meio do algoritimo com a atribuição
+
+teste = "Isadora"
+print (teste) # imprime a string "Isadora"
+print (type(teste)) # imprime o tipo da variavel, string
+teste = 10
+print (teste) # imprime o numero 10
+print (type (teste)) #imprime o tipo da variavel, que é int
+teste = 1.64
+print (teste) # imprime o numero 1.64
+print (type(teste)) # imprime o tipo da variavel, que é float
+teste = True 
+print (teste) # imprime o valor da true 
+print (type(teste)) # imprime o tipo da variavel, que é bool
