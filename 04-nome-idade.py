5df76c27ee3e5ebccca91b23b4519efbc4078ed3
Isadora Amaral <isadoraferreiraamaraldasilva@gmail.com>
2024-08-29 11:50:00 -0300

    Add files via upload

---
+++ 04-nome-idade.py
@@ -0,0 +1,15 @@
+# Atribuiçõe de variáveis
+nome = "Isadora"
+idade = 16
+altura = 1.64
+
+# Exibir mensagem na tela 
+print("nome: ", nome, " idade ", idade, " altura ", altura)
+# formação com f-string
+print(f"nome: {nome} idade {idade} altura {altura}")
+
+nome = input("digite o seu nome: ")
+idade = int(input("digite a sua idade: "))
+altura = float(input("digite a sua altura: "))
+
+print(f"nome: {nome} idade: {idade} altura: {altura}")
